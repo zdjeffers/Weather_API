03a215ba001ff57ed404503096e6b27c4a3d7fbd
Dave Jeffers <zdjeffers@gmail.com>
2021-01-16 09:28:37 -0500

    updated CSS & JS file

--- Develop/script.js
+++ Develop/script.js
@@ -2,8 +2,30 @@
 
 var citiesSearched = [];
 
-var weatherAPI = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=";
+var weatherAPI = "http://api.openweathermap.org/data/2.5/weather?q=";
 
-var APIKey = "cedfe23bde01de8030da3c90beae4ba5";
+const APIKey = "&appid=cedfe23bde01de8030da3c90beae4ba5";
 
-var currentWeather
+// $(document).ready(function() {
+//     let savedSearches = JSON.parse(localStorage.getItem("pastSearches")) || [];
+// })
+
+
+function displayWeather () {
+    var search = $("#searchForm").text.attr("data-name");
+    console.log(search)
+    var queryURL = weatherAPI + search + APIKey;
+    $.ajax ({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        var currentCity = response.name
+        console.log(currentCity);
+        $(".currentCity").text(currentCity)
+    })
+}
+
+var searchTerm = $()
+
+$(document).on("click", "#searchBtn", displayWeather);
+
