3149d6581ad3ff1069205cf3d46a3c84f8cd1d88
Dave Jeffers <zdjeffers@gmail.com>
2021-01-16 17:35:36 -0500

    Added AJAX calls for current weather

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,10 @@
 //Define Variables
 
-var citiesSearched = [];
-
 var weatherAPI = "http://api.openweathermap.org/data/2.5/weather?q=";
-
 const APIKey = "&appid=cedfe23bde01de8030da3c90beae4ba5";
+
+var todaysDate = moment().format('l');
+
 
 // $(document).ready(function() {
 //     let savedSearches = JSON.parse(localStorage.getItem("pastSearches")) || [];
@@ -12,20 +12,35 @@ const APIKey = "&appid=cedfe23bde01de8030da3c90beae4ba5";
 
 
 function displayWeather () {
-    var search = $("#searchForm").text.attr("data-name");
-    console.log(search)
-    var queryURL = weatherAPI + search + APIKey;
+    let searchTerm = $("#searchForm").val().trim();
+    var queryURL = weatherAPI + searchTerm + APIKey;
     $.ajax ({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-        var currentCity = response.name
+        let currentCity = response.name
+        $(".currentCity").text(currentCity).append(" " + todaysDate);
+
+        let currentTemperature = Math.floor(((response.main.temp - 273.15) * (9/5) + 32));
+        $(".currentTemperature").text(currentTemperature);
+
+        let currentHumidity = response.main.humidity;
+        $(".currentHumidity").text(currentHumidity);
+
+        let currentWindSpeed = response.wind.speed;
+        $(".currentWindSpeed").text(currentWindSpeed);
+
+        let currentUVIndex = 
+
         console.log(currentCity);
-        $(".currentCity").text(currentCity)
+        
     })
 }
 
-var searchTerm = $()
 
-$(document).on("click", "#searchBtn", displayWeather);
+
+$("#searchBtn").on("click", function(event) {
+    event.preventDefault();
+    displayWeather();
+});
 
