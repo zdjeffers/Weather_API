73f250c1ffc0e2681725f09a0eefb3f8d899f482
Dave Jeffers <zdjeffers@gmail.com>
2021-01-15 20:25:22 -0500

    Added CSS elements

---
+++ Develop/script.js
@@ -0,0 +1,9 @@
+//Define Variables
+
+var citiesSearched = [];
+
+var weatherAPI = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=";
+
+var APIKey = "cedfe23bde01de8030da3c90beae4ba5";
+
+var currentWeather
